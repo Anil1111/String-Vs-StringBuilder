1f8ee9f066ba1a3965817b829285319efccea1a4
anilsri.04@Gmail.com <anilsri.04@Gmail.com>
2019-03-29 07:57:33 +0530

    String Vs StringBuilder

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -10,6 +10,24 @@ namespace ConsoleApp7
     {
         static void Main(string[] args)
         {
+            string fName = "Anil";
+            string lName = "Srivastava";
+            string statement = "Hello, my name is ";
+            string sentence = statement + fName + " " + lName + "!";
+            Console.WriteLine(sentence);
+
+            sentence = sentence.ToUpper();
+            Console.WriteLine(sentence);
+
+            StringBuilder paragraph = new StringBuilder();
+            paragraph.Append("\nHello, my name is Anil.");
+            paragraph.Append(" I enjoy coding using C#.");
+            paragraph.Append(" It has been an excellent language to learn.");
+            paragraph.Append(" I think stringBuilder is better than string!");
+            Console.WriteLine(paragraph);
+
+
+            Console.ReadLine();
         }
     }
 }
